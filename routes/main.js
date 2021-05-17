8c89db533601108ec1963b22d3710bd737607d89
hzk2021 <hezhenkai1999@gmail.com>
2021-05-17 21:23:31 +0800

    Added GET register and login page routes

--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,20 @@ const Express = require('express');
 const Router = Express.Router();
 
 Router.get('/', (req,res) => {
-    res.render('about', {
+    res.render('index', {
         author: 'zk, yantat, nigel',
         success_msg: "Hello World!!", // example success
         error_msg: "Error !!" // example error
 
     })
 })
+
+Router.get('/showLogin', (req,res) => {
+    res.render('user/login');
+});
+
+Router.get('/showRegister', (req,res) => {
+    res.render('user/register');
+});
 
 module.exports = Router;
