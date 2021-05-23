5d786eddb885286a23a9329ae625175eecb283cd
hzk2021 <hezhenkai1999@gmail.com>
2021-05-23 22:09:33 +0800

    Add example for connect-flash and session store

--- routes/main.js
+++ routes/main.js
@@ -6,8 +6,7 @@ Router.get('/', (req,res) => {
         author: 'zk, yantat, nigel',
         success_msg: "Hello World!!", // example success
         error_msg: "Error !!" // example error
-
-    })
+    });
 })
 
 Router.get('/showLogin', (req,res) => {
@@ -17,5 +16,21 @@ Router.get('/showLogin', (req,res) => {
 Router.get('/showRegister', (req,res) => {
     res.render('authentication/register');
 });
+
+// Connect flash & session store example
+// Router.get('/', (req,res) => {
+//     res.render('index', {
+//         author: 'zk, yantat, nigel',
+//         success_msg: req.flash('success_msg'), // connect flash example success
+//         error_msg: "Error !!" // example error
+//     })
+// })
+
+// Router.get('/showLogin', (req,res) => {
+//     req.session.test = true; // Will also be stored in mysql db
+//     console.log(req.session);
+//     req.flash('success_msg', 'idk');
+//     res.redirect('/');
+// });
 
 module.exports = Router;
