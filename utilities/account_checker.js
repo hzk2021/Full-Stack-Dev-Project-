fbb849e3c9d9e552c70d46efae8b388875b90bc4
hzk2021 <hezhenkai1999@gmail.com>
2021-08-14 23:31:19 +0800

    Added supplier role

--- utilities/account_checker.js
+++ utilities/account_checker.js
@@ -70,4 +70,25 @@ const isAdmin = function (req, res, next) {
 	}
 }
 
-module.exports = { isLoggedIn, isNotLoggedIn, isAdmin, isUser};
+/**
+ * 
+ * @param {Express.Request} req Express Request handle
+ * @param {Express.Response} res Express Response handle
+ */
+ const isSupplier = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		if (req.user.role == UserRole.Supplier) {
+			return next();
+		}
+		else {
+			console.log(`Current user(supplier) is not privileged to access user portal: ${req.user.uuid}`);
+			return res.sendStatus(403).end();
+		}
+	} 
+	else {
+		console.log(`Either session expired or not logged in`);
+		return res.redirect('/auth/login');
+	}
+}
+
+module.exports = { isLoggedIn, isNotLoggedIn, isAdmin, isUser, isSupplier};
